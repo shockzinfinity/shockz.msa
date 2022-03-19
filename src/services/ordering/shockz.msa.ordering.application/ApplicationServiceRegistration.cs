a58b0bf9c025946f0d761d0c146fff07a4226d61
shockz <shockzinfinity@gmail.com>
2022-03-19 18:31:39 +0900

    add application contracts

---
+++ src/services/ordering/shockz.msa.ordering.application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using shockz.msa.ordering.application.Behaviors;
+using System.Reflection;
+
+namespace shockz.msa.ordering.application
+{
+  public static class ApplicationServiceRegistration
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+      services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+      services.AddMediatR(Assembly.GetExecutingAssembly());
+
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+      return services;
+    }
+  }
+}
