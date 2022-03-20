50e6651e1e332fceaea5edb741286d5fa4b7a2c2
shockz <shockzinfinity@gmail.com>
2022-03-20 14:10:18 +0900

    containerization

--- src/services/ordering/shockz.msa.ordering.application/Behaviors/UnhandledExceptionBehavior.cs
+++ src/services/ordering/shockz.msa.ordering.application/Behaviors/UnhandledExceptionBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
+using shockz.msa.ordering.application.Exceptions;
 
 namespace shockz.msa.ordering.application.Behaviors
 {
@@ -19,6 +20,14 @@ namespace shockz.msa.ordering.application.Behaviors
       } catch (Exception ex) {
         var requestName = typeof(TRequest).Name;
         _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+
+        if(ex is ValidationException) {
+          foreach (var (error, i) in (ex as ValidationException).Errors.Select((error, i) => (error, i))) {
+            // NOTE: below code has performace issue.
+            //_logger.LogError($"{i + 1}. {error.Key} {error.Value.Aggregate((t, x) => t + '|' + x)}");
+            _logger.LogError($"{i + 1}. {error.Key}: {string.Join(" | ", error.Value)}");
+          }
+        }
         throw;
       }
     }
