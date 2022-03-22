3dba93a4dd6b2e82d17fbb0379c0d35bc5a75fb7
shockz <shockzinfinity@gmail.com>
2022-03-22 18:23:41 +0900

    gateway containerize

--- src/apiGateways/shockz.msa.ocelotApiGw/Program.cs
+++ src/apiGateways/shockz.msa.ocelotApiGw/Program.cs
@@ -1,19 +1,19 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 // ocelot.Development.json or ocelot.Local.json
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
-//builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-//builder.Logging.AddConsole();
-//builder.Logging.AddDebug();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", () => "Hello there.");
 await app.UseOcelot(); // middleware
 
 app.Run();
