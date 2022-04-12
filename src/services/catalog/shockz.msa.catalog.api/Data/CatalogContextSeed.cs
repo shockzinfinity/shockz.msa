d978d19b095601761291f76fbd6be730671594a6
shockz <shockzinfinity@gmail.com>
2022-04-12 13:41:40 +0900

    include price in product entity

--- src/services/catalog/shockz.msa.catalog.api/Data/CatalogContextSeed.cs
+++ src/services/catalog/shockz.msa.catalog.api/Data/CatalogContextSeed.cs
@@ -113,7 +113,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-1.png",
-          //Price = 950.00M,
+          Price = 950.00M,
           Category = "Smart Phone"
         },
         new Product()
@@ -123,7 +123,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-2.png",
-          //Price = 840.00M,
+          Price = 840.00M,
           Category = "Smart Phone"
         },
         new Product()
@@ -133,7 +133,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-3.png",
-          //Price = 650.00M,
+          Price = 650.00M,
           Category = "White Appliances"
         },
         new Product()
@@ -143,7 +143,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-4.png",
-          //Price = 470.00M,
+          Price = 470.00M,
           Category = "White Appliances"
         },
         new Product()
@@ -153,7 +153,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-5.png",
-          //Price = 380.00M,
+          Price = 380.00M,
           Category = "Smart Phone"
         },
         new Product()
@@ -163,7 +163,7 @@ namespace shockz.msa.catalog.api.Data
           Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
           Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
           ImageFile = "product-6.png",
-          //Price = 240.00M,
+          Price = 240.00M,
           Category = "Home Kitchen"
         }
       };
