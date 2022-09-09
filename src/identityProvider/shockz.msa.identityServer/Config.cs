4ac4a0f112cdcf7cb2b2f3bb1725c291a66549a7
shockz <shockzinfinity@gmail.com>
2022-09-09 11:39:37 +0900

    add client, api scope

--- src/identityProvider/shockz.msa.identityServer/Config.cs
+++ src/identityProvider/shockz.msa.identityServer/Config.cs
@@ -7,10 +7,18 @@ namespace shockz.msa.identityServer
   {
     public static IEnumerable<Client> Clients => new Client[]
     {
+      new Client
+      {
+        ClientId = "movieClient",
+        AllowedGrantTypes = GrantTypes.ClientCredentials,
+        ClientSecrets = { new Secret("secret".Sha256()) },
+        AllowedScopes = { "movieAPI" }
+      }
     };
 
     public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
     {
+      new ApiScope("movieAPI", "Movie API")
     };
 
     public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
--- src/identityProvider/shockz.msa.identityServer/Program.cs
+++ src/identityProvider/shockz.msa.identityServer/Program.cs
@@ -4,10 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServer()
   .AddInMemoryClients(Config.Clients)
-  .AddInMemoryIdentityResources(Config.IdentityResources)
-  .AddInMemoryApiResources(Config.ApiResources)
   .AddInMemoryApiScopes(Config.ApiScopes)
-  .AddTestUsers(Config.TestUsers)
   .AddDeveloperSigningCredential();
 
 var app = builder.Build();
